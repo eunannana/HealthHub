14b41696bbf6ed5973978bb20db95dceecad2722
ahmadmm01 <ahmad.musthafa.ft20@mail.umy.ac.id>
2023-06-24 19:57:09 +0700

    fix bug error

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -7,10 +7,10 @@ class AuthController {
   final CollectionReference usersCollection =
       FirebaseFirestore.instance.collection('users');
 
-  Future<UserModel?> createUserWithEmailAndPassword(
-      String email, String password, String name, DateTime dob, String gender) async {
+  Future<UserModel?> createUserWithEmailAndPassword(String email,
+      String password, String name, DateTime dob, String gender) async {
     try {
-       final QuerySnapshot snapshot = await usersCollection
+      final QuerySnapshot snapshot = await usersCollection
           .where('uEmail', isEqualTo: email)
           .limit(1)
           .get();
@@ -18,15 +18,21 @@ class AuthController {
       if (snapshot.docs.isNotEmpty) {
         throw Exception('Email is already registered');
       }
-      final UserCredential userCredential = await auth.createUserWithEmailAndPassword(email: email,
-      password: password,
+      final UserCredential userCredential =
+          await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
       );
       final User? user = userCredential.user;
-      
+
       // Update user profile with display name
-       if (user != null) {
-        final UserModel newUser =
-            UserModel(uName: name, uEmail: user.email ?? '', uId: user.uid, uDateOfBirth: dob, uGender: gender);
+      if (user != null) {
+        final UserModel newUser = UserModel(
+            uName: name,
+            uEmail: user.email ?? '',
+            uId: user.uid,
+            uDateOfBirth: dob,
+            uGender: gender);
         await usersCollection.doc(newUser.uId).set(newUser.toMap());
         return newUser;
       }
@@ -34,13 +40,13 @@ class AuthController {
       print('Error registering user: $e');
     }
     return null;
-    }
   }
-  
-  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
     try {
-      
-      final UserCredential userCredential = await auth.signInWithEmailAndPassword(
+      final UserCredential userCredential =
+          await auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
@@ -52,6 +58,4 @@ class AuthController {
       return null;
     }
   }
-  
-  
-
+}
