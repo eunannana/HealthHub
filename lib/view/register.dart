c9baa36348318dda1759b60ca5d754db33dac600
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2023-06-27 21:59:20 +0700

    modify register.dart

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:healthhub/controller/auth_controller.dart';
@@ -19,8 +17,11 @@ class _RegisterViewState extends State<RegisterView> {
   String? name;
   String? email;
   String? password;
+  String? rePassword;
   String? gender;
   String? dob;
+  bool hidePassword = true;
+  bool hideRePassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -29,164 +30,247 @@ class _RegisterViewState extends State<RegisterView> {
         title: const Text('Register'),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Form(
-            key: formkey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Name'),
-                  onChanged: (value) {
-                    name = value;
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name.';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Email'),
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email.';
-                    }
-                    if (!RegExp(
-                            r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
-                        .hasMatch(value)) {
-                      return 'Please enter a valid email address.';
-                    }
-                    return null;
-                  },
-                ),
-                DropdownButtonFormField<String>(
-                  decoration: const InputDecoration(hintText: 'Gender'),
-                  value: gender,
-                  onChanged: (value) {
-                    gender = value;
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please select your gender.';
-                    }
-                    return null;
-                  },
-                  items: genderOptions.map((String option) {
-                    return DropdownMenuItem<String>(
-                      value: option,
-                      child: Text(option),
-                    );
-                  }).toList(),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.calendar_today),
-                  title: Text(
-                    dob != null
-                        ? DateFormat('dd MMMM yyyy').format(DateTime.parse(dob!))
-                        : 'Select Date of Birth',
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Form(
+              key: formkey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      hintText: 'Enter your name',
+                    ),
+                    onChanged: (value) {
+                      name = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name.';
+                      }
+                      return null;
+                    },
                   ),
-                  onTap: () {
-                    selectDate(context);
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Password'),
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password.';
-                    }
-                    if (value.length < 6) {
-                      return 'Password must be at least 6 characters.';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    if (formkey.currentState!.validate()) {
-                      try {
-                        UserModel? registeredUser =
-                            await userCr.createUserWithEmailAndPassword(
-                                email!, password!, name!, dob!, gender!);
-                        if (registeredUser != null) {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Registration Successful'),
-                                content: const Text(
-                                    'You have been successfully registered.'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              );
-                            },
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                    ),
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email.';
+                      }
+                      if (!RegExp(
+                              r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                  ),
+                  DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelText: 'Gender',
+                      hintText: 'Select your gender',
+                    ),
+                    value: gender,
+                    onChanged: (value) {
+                      gender = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select your gender.';
+                      }
+                      return null;
+                    },
+                    items: genderOptions.map((String option) {
+                      return DropdownMenuItem<String>(
+                        value: option,
+                        child: Text(option),
+                      );
+                    }).toList(),
+                  ),
+                  Row(
+  children: [
+    Text(
+      'Date of Birth',
+      style: TextStyle(
+        fontSize: 16,
+      ),
+    ),
+    SizedBox(width: 10),
+    IconButton(
+      onPressed: () {
+        selectDate(context);
+      },
+      icon: Icon(
+        Icons.calendar_today,
+        color: Theme.of(context).primaryColor,
+      ),
+    ),
+    TextButton(
+      onPressed: () {
+        selectDate(context);
+      },
+      child: Text(
+        dob != null
+            ? DateFormat('dd MMMM yyyy').format(DateTime.parse(dob!))
+            : 'Select Date of Birth',
+      ),
+    ),
+  ],
+),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            hidePassword = !hidePassword;
+                          });
+                        },
+                        icon: Icon(
+                          hidePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    obscureText: hidePassword,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password.';
+                      }
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters.';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Re-Password',
+                      hintText: 'Re-enter your password',
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            hideRePassword = !hideRePassword;
+                          });
+                        },
+                        icon: Icon(
+                          hideRePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      rePassword = value;
+                    },
+                    obscureText: hideRePassword,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please re-enter your password.';
+                      }
+                      if (value != password) {
+                        return 'Passwords do not match.';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (formkey.currentState!.validate()) {
+                        try {
+                          UserModel? registeredUser =
+                              await userCr.createUserWithEmailAndPassword(
+                            email!,
+                            password!,
+                            name!,
+                            dob!,
+                            gender!,
                           );
-                        }
-                      } catch (e) {
-                        if (e
-                            .toString()
-                            .contains('Email is already registered')) {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Registration Failed'),
-                                content:
-                                    const Text('Email is already registered.'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('OK'),
+                          if (registeredUser != null) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Registration Successful'),
+                                  content: const Text(
+                                    'You have been successfully registered.',
                                   ),
-                                ],
-                              );
-                            },
-                          );
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Registration Failed'),
-                                content: const Text(
-                                    'An error occurred during registration.'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        } catch (e) {
+                          if (e.toString()
+                              .contains('Email is already registered')) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Registration Failed'),
+                                  content: const Text(
+                                      'Email is already registered.'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Registration Failed'),
+                                  content: const Text(
+                                      'An error occurred during registration.'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
                         }
                       }
-                    }
-                  },
-                  child: const Text('Register'),
-                ),
-              ],
+                    },
+                    child: const Text('Register'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -198,7 +282,7 @@ class _RegisterViewState extends State<RegisterView> {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime(1900),
+      firstDate: DateTime(1950), // Batas awal pemilihan tahun
       lastDate: DateTime.now(),
     );
     if (picked != null) {
