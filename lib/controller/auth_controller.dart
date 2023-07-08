5b27d879d827be6f728fa31419eceb2f0c8bed57
ahmadmm01 <ahmad.musthafa.ft20@mail.umy.ac.id>
2023-07-09 00:55:21 +0700

    add intiial userdata for user

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: avoid_print
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:healthhub/model/user_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthController {
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -18,6 +20,7 @@ class AuthController {
       if (snapshot.docs.isNotEmpty) {
         throw Exception('Email is already registered');
       }
+
       final UserCredential userCredential =
           await auth.createUserWithEmailAndPassword(
         email: email,
@@ -33,7 +36,30 @@ class AuthController {
             uId: user.uid,
             uDateOfBirth: dob,
             uGender: gender);
+
+        final DateTime now = DateTime.now();
+        final String formattedDate =
+            '${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}';
+
+        final Map<String, dynamic> initialSuccessPoint = {
+          'uHydrationLevel': 0,
+          'uHydrationPoint': 0,
+          'uExerciseDuration': 0,
+          'uExercisePoint': 0,
+          'uCalorieCount': 0,
+          'uCaloriePoint': 0,
+          'uSleepDuration': 0,
+          'uSleepPoint': 0,
+          'uSuccessPoint': 0,
+        };
+
         await usersCollection.doc(newUser.uId).set(newUser.toMap());
+        await usersCollection
+            .doc(newUser.uId)
+            .collection('uDailysuccesspoint')
+            .doc(formattedDate)
+            .set(initialSuccessPoint);
+
         return newUser;
       }
     } catch (e) {
@@ -42,7 +68,7 @@ class AuthController {
     return null;
   }
 
-Future<UserModel?> signInWithEmailAndPassword(
+  Future<UserModel?> signInWithEmailAndPassword(
       String email, String password) async {
     try {
       final UserCredential userCredential = await auth
@@ -56,6 +82,39 @@ Future<UserModel?> signInWithEmailAndPassword(
         if (snapshot.exists) {
           final UserModel loggedInUser =
               UserModel.fromMap(snapshot.data() as Map<String, dynamic>);
+
+          final DateTime now = DateTime.now();
+          final String formattedDate =
+              '${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}';
+
+          final DocumentSnapshot dailySuccessPointSnapshot =
+              await usersCollection
+                  .doc(loggedInUser.uId)
+                  .collection('uDailysuccesspoint')
+                  .doc(formattedDate)
+                  .get();
+
+          if (!dailySuccessPointSnapshot.exists) {
+            // Inisialisasi daily success point dengan data kosong
+            final Map<String, dynamic> initialSuccessPoint = {
+              'uHydrationLevel': 0,
+              'uHydrationPoint': 0,
+              'uExerciseDuration': 0,
+              'uExercisePoint': 0,
+              'uCalorieCount': 0,
+              'uCaloriePoint': 0,
+              'uSleepDuration': 0,
+              'uSleepPoint': 0,
+              'uSuccessPoint': 0,
+            };
+
+            await usersCollection
+                .doc(loggedInUser.uId)
+                .collection('uDailysuccesspoint')
+                .doc(formattedDate)
+                .set(initialSuccessPoint);
+          }
+
           return loggedInUser;
         }
       }
@@ -64,5 +123,4 @@ Future<UserModel?> signInWithEmailAndPassword(
     }
     return null;
   }
-  
 }
